ba4529e29caa2065656a037a542cf0aee8431f17
David Trussler <mail@davidtrussler.net>
2024-11-22 20:14:25 +0000

    Complete Sign-up
    Completes the work to add the user sign-up functionality
    - Add debug section
    - Add Users resource and view to display users
    - Add gravatar image and sidebar
    - Add signup form to view
    - Add create action to controller
    - Add error messaging
    - Add test for user signup
    - Handle successful signup
    - Update test for user signup to test valid signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "Invalid signup information" do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "short",
+          password_confirmation: "long"
+        }
+      }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+  end
+
+  test "Valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: "David Trussler",
+          email: "user@valid.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
