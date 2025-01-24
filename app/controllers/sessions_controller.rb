3f7667b2e79ae4a2fd01c23f2c20aa59373ca46c
davidtrussler <mail@davidtrussler.net>
2025-01-24 16:39:25 +0000

    Add `create` action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticaate(params[:session][:password])
+      # Log in user
+    if test
+      # Create error message
+    else
+      render 'new', status: :unprocessable_entity
+    end
+
+    # debugger
+  end
+
+  def destroy
+  end
 end
