155450daef1d8ea50985c834bc433dd3061e36af
davidtrussler <mail@davidtrussler.net>
2025-02-07 16:00:57 +0000

    Add markup, CSS, JS for responsive layout

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,5 +1,13 @@
 document.addEventListener("turbo:load", function() {
+  let hamburger = document.querySelector("#hamburger");
   let account = document.querySelector("#account")
+
+  hamburger.addEventListener("click", function(event) {
+    let menu = document.querySelector("#navbar-menu");
+
+    event.preventDefault();
+    menu.classList.toggle("collapse");
+  });
 
   account.addEventListener("click", function(event) {
     let menu = document.querySelector("#dropdown-menu");
