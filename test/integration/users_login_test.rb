8c91064612caf4e89bbf98d488b4a25aa1d696e2
davidtrussler <mail@davidtrussler.net>
2025-04-11 16:28:54 +0100

    Fix bug whereby user can log out in one browser window while remaining logged in in another - Update users_login_test to simulate a user clicking logout in a second window - Update `destroy` method in sessions_controller to only log the usesr out if they are logged in

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -48,6 +48,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_response :see_other
     assert_redirected_to root_url
 
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
+
     follow_redirect!
 
     assert_select "a[href=?]", login_path
